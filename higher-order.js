8d903d1fa3d9559fbd3fc97e1904498430aa2549
Dennis Murray <murraden@gmail.com>
2023-12-11 19:49:32 -0800

    Week 9 Homework for Dennis Murray

--- higher-order.js
+++ higher-order.js
@@ -1,7 +1,23 @@
 // This function should execute the callback function the number of times specified.
 // When the function is being executed, the repetition number (i.e. 1 for the first call)
 // should be passed to the callback.
-const repeatFn = (times, callback) => {};
+const repeatFn = (num, callback) => {
+
+  for (let i = 0; i < num; i++) {
+    callback(i)
+  }
+
+};
+
+repeatFn(5, (param) => {
+  console.log(param)
+})
+
+const doubleNumber = (num) => {
+  console.log(num * 2)
+}
+
+repeatFn(5, doubleNumber)
 
 // Test repeatFn
 const addButton = num => {
